66f5a3d28ff22ed2ec299ab25865b2edb9f95644
mahi321k <maheshwarreddykyatham079@gmail.com>
2025-08-16 14:12:14 +0530

    going around all axis

--- Coordinates/orientation.py
+++ Coordinates/orientation.py
@@ -11,7 +11,7 @@ Ny_axis = arrow(pos=vector(0,0,0), axis=vector(0,-1,0), color=color.green, shaft
 Nz_axis = arrow(pos=vector(0,0,0), axis=vector(0 ,0,-1), color=color.blue, shaftwidth=0.02,length=5)
 
 myVecMag = 3.0
-myVecAng = 120  # degrees
+myVecAng = 0  # degrees
 myVecXmag = myVecMag * np.cos(np.radians(myVecAng))
 myVecYmag = myVecMag * np.sin(np.radians(myVecAng))
 myVecZ = 0  # assuming a 2D vector in the XY plane
@@ -25,16 +25,35 @@ myVecRes = arrow(pos=vector(0,0,0), axis=vector(myVecXmag,myVecYmag,0), color=co
 
 print("myVecXmag:", sign(myVecXmag))
 print("myVecYmag:", sign(myVecYmag))
+
+xy=1
+yz=0
+zy=0
+
 while True:
     # pass
-    rate(30)
+    rate(150)
     myVecAng = myVecAng + 1
     myVecXmag = myVecMag * np.cos(np.radians(myVecAng))
     myVecYmag = myVecMag * np.sin(np.radians(myVecAng))
-    myVecRes.axis = vector(myVecXmag, myVecYmag, 0)
+    
+    if xy == 1:
+        myVecRes.axis = vector(myVecXmag, myVecYmag, 0)
+        if myVecAng >= 360:
+            myVecRes.axis = vector(myVecXmag,0, myVecYmag)
+            if myVecAng >= 810:
+                myVecRes.axis = vector(0, -myVecXmag, myVecYmag)
+                if myVecAng >= 1170:
+                    myVecRes.axis = vector(-myVecXmag, 0, myVecYmag)
+                    if myVecAng >= 1260:
+                        myVecAng = 0
 
-    myVecX.pos = vector(0,myVecYmag,0)
-    myVecX.axis = vector(myVecXmag, 0, 0)
-    myVecY.pos = vector(myVecXmag, 0, 0)
-    myVecY.axis = vector(0, myVecYmag, 0)
+
+
+
+
+    # myVecX.pos = vector(0,myVecYmag,0)
+    # myVecX.axis = vector(myVecXmag, 0, 0)
+    # myVecY.pos = vector(myVecXmag, 0, 0)
+    # myVecY.axis = vector(0, myVecYmag, 0)
 
